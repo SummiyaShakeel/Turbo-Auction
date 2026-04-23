562f158a3d0f32d0f64b9cfdebcaa1560cec540f
Laraib Hafeez <laraib3655@gmail.com>
2026-04-23 16:48:09 +0500

    Update Program.cs

--- Shared/Program.cs
+++ Shared/Program.cs
@@ -1,1 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
+namespace Project_cars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("cars.json");
+
+            List<Car> carList = JsonConvert.DeserializeObject<List<Car>>(json);
+
+            foreach (Car car in carList)
+            {
+                Console.WriteLine("Brand: " + car.Brand);
+                Console.WriteLine("Model: " + car.Model);
+                Console.WriteLine("Year: " + car.Year);
+                Console.WriteLine("Price: $" + car.StartingPrice);
+                Console.WriteLine("Image: " + car.ImagePath);
+                Console.WriteLine("-----------------------------------");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
